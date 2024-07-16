3e79dcc0f5592460ced5c9efaacea562f58fa87b
Muhamad Faiz Riyadi <muhammadfaizriyadi@gmail.com>
2024-07-16 13:22:01 +0700

    add cors again

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const usersRouter = require('./routes/usersRoutes');
 const placeRoutes = require('./routes/placeRoutes');
 const favoritesRouter = require('./routes/favoritesRoutes');
 const userReviewRoutes = require('./routes/userReviewsRoutes');
+
+// Enable CORS for all routes
+app.use(cors());
 
 app.use(express.json());
 
